285388ab7af19204b6b9d2be6f13f6386f4704e7
olivermmoss <omoss5040@gmail.com>
2025-06-05 20:05:20 -0400

    added code to find minimum amount of convex pentagons

---
+++ bankers.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Bit string length (specified in program args).
+ */
+static unsigned int length;
+
+/*
+ * Print binary representation of b.
+ * Use `.' instead of `0' for readability.
+ */
+void output (unsigned long b)
+{
+    unsigned int i;
+    for (i = 0; i < length; i++) {
+        if (b & 1)
+            printf("1");
+        else
+            printf(".");
+        b = b >> 1;
+    }
+    printf("\n");
+}
+
+/*
+ * Compute the binomial coefficient of `n choose k'.
+ * Use the fact that binom(n, k) = binom(n, n - k).
+ * Use a lookup table (triangle, actually) for speed.
+ * Otherwise it's dumb (heart) recursion.
+ */
+unsigned long choose (unsigned int n, unsigned int k) {
+    static unsigned long *quick;
+    if (n == 0)
+        return 0;
+    if (n == k || k == 0)
+        return 1;
+    if (k > n / 2)
+        k = n - k;
+
+    if (!quick)
+        quick = (unsigned long*)calloc((length * (length + 1)) / 4, sizeof(unsigned long));
+
+    unsigned int i = (n * (n - 1)) / 4 + k - 1;
+    if (quick[i] == 0)
+        quick[i] = choose(n - 1, k - 1) + choose(n - 1, k);
+
+    return quick[i];
+}
+
+/*
+ * Returns the Banker's number at the specified position, a.
+ * Derived from the recursive bit flip method.
+ */
+unsigned long compute (unsigned long a)
+{
+    unsigned long b = 0;
+    if (a == 0)
+        return b;
+
+    unsigned int c = 0, n = length;
+    unsigned long e = a, binom;
+    binom = choose(n, c);
+    do {
+        e -= binom;
+    } while ((binom = choose(n, ++c)) <= e);
+
+    do {
+        if (e == 0 || (binom = choose(n - 1, c - 1)) > e)
+            c--, b |= 1;
+        else
+            e -= binom;
+    } while (--n && c && ((b <<= 1) || 1));
+    b <<= n;
+
+    return b;
+}
+
+/*
+ * Returns the position of the specified Banker's number, b.
+ * The ones contribute \sum_{i=0}^{c-1}{\binom{n}{i}}.
+ * Each zero (except leading zeros) contributes
+ *  \binom{n-i-1}{c-c_i-1}, where
+ *   n is the bit string length,
+ *   c is the cardinality of the bit string,
+ *   c_i is the cardinality up to bit i, and
+ *   i is the position of the zero in the string.
+ */
+unsigned long inverse (unsigned long b) {
+    unsigned int c = 0, n = length;
+    unsigned long a = 0, i = 1;
+    for (; i <= b; i <<= 1)
+        if (b & i) c++;
+    for (i = 1 << (n - 1); i > 0 && c > 0; --n, i >>= 1)
+        if (b & i)
+            a += choose(length, --c);
+        else
+            a += choose(n - 1, c - 1);
+
+    return a;
+}
+
+/*
+ * Recursive function
+ */
+unsigned long next (unsigned long b)
+{
+    unsigned int z = 0, y = 0;
+    unsigned long i = 1, max = 1 << length;
+    while (i < max && b & i)
+        y++, i <<= 1;
+    while (i < max && !(b & i))
+        z++, i <<= 1;
+    if (i < max) {
+        b &= ~i;
+        i = ~((1 << (z + y + 1)) - 1);
+    }
+    b &= i;
+    i = (1 << (y + z)) - (1 << (z - 1));
+    b |= i;
+    return b & max - 1;
+}
+
+/* 
+ * Main program accepts one parameter: the number of bits
+ * in the bit string.
+ */
+/*int main (int argc, char ** argv)
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s n\n", argv[0]);
+        exit(1);
+    }
+    length = atoi(argv[1]);
+    unsigned long b = 0;
+    for(;;) {
+				if(1093 <= inverse(b) && inverse(b) <= 2379) {
+        	printf("%lu: ", inverse(b));
+        	output(compute(inverse(b)));
+				}
+        if (b == (1 << length) - 1) break;
+        b = next(b);
+    }
+    for (b = 0; b < 1 << length; b++) {
+				if(1093 <= b && b <= 2379) {
+        	printf("%lu: ", inverse(compute(b)));
+        	output(compute(b));
+				}
+    }
+    return 0;
+}*/
---
+++ minpconvexk5.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bankers.c"
+
+// number of vertices
+int N = 13;
+unsigned long Nchoose5 = (N * (N-1) * (N-2) * (N-3) * (N-4)) / 120;
+
+// searching for at most/exactly this many pseudoconvex pentagons
+int k = 4;
+bool exact = true;
+
+int minBankers;
+
+// p(i,j) is whether the edge btwn i and j is inside or outside.
+// note that we only use this when i < j - 1, otherwise it's a useless var
+// but encoding it this way is way easier lol - we just set useless vars to true
+int p(int i, int j) {
+	// plus one is to move it from [0, N) to [1, N]
+	return i * N + j + 1;
+}
+
+// get i given p
+int pi(int p) {
+	return (p - 1) / N;
+}
+
+// get j given p
+int pj(int p) {
+	return (p - 1) % N;
+}
+
+// q(a,b,c,d,e) is whether those vtxs are a pseudo-convex pentagon
+int q(int a, int b, int c, int d, int e) {
+	// don't think I need to do any casting as long as N < 31
+	unsigned long pos = ((1 << a) + (1 << b) + (1 << c) + (1 << d) + (1 << e));
+	
+	return 1 + N*N + inverse(pos) - minBankers;
+}
+
+//got this literally from the google AI output, just searched "C write to file skeleton code"
+int main() {
+	length = N;
+	minBankers = inverse((1 << N-1) + (1 << N-2) + (1 << N-3) + (1 << N-4) + (1 << N-5));
+
+	// testing choose (I don't like the outputs but they work???)
+	/*printf("%lu\n", Nchoose5);
+	printf("%lu\n", choose(N, 5));
+	printf("%lu\n", choose(13, 5));
+	printf("%d\n", (13*12*11*10*9)/120);*/
+
+  FILE *file_pointer;
+  const char *filename = "mink5.knf"; // Specify the name of the file
+
+  // Open the file in write mode ("w"). If the file doesn't exist, it will be created.
+  // If the file exists, it will be overwritten.
+  file_pointer = fopen(filename, "w");
+
+  if (file_pointer == NULL) {
+    printf("Error opening the file.\n");
+    return 1; // Indicate an error
+  }
+
+  // Write data to the file
+	int Nchoose2 = (N * (N-1)) / 2;
+	unsigned long Nclauses = (2*N + 2*Nchoose2 + 22*Nchoose5 + 1 + 1);
+	if(exact) Nclauses++;
+
+  fprintf(file_pointer, "p knf %lu %lu\n", N*N + Nchoose5, Nclauses);
+	// it's + 2*(Nchoose2 - N + 1) if we're doing symmetry  
+
+	// first, we set useless vars to true:
+	// p(i,j), is true arbitrarily when j = i or i + 1 (note this takes care of (0, n-1) because of symmetry below
+	for(int i = 0; i < N; i++) {
+			fprintf(file_pointer, "%d 0\n", p(i,i));
+			fprintf(file_pointer, "%d 0\n", p(i,(i+1) % N));
+	}
+
+	// p(i,j) = p(j,i)
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < i; j++) {
+			fprintf(file_pointer, "%d %d 0\n", p(i,j), -p(j,i));
+			fprintf(file_pointer, "%d %d 0\n", -p(i,j), p(j,i));
+	}}
+
+	// Now we add the clauses: want to loop through every group of 5
+	for(int a = 0; a < N - 4; a++) {
+		for(int b = a+1; b < N - 3; b++) {
+			for(int c = b+1; c < N - 2; c++) {
+				for(int d = c+1; d < N - 1; d++) {
+					for(int e = d+1; e < N; e++) {
+						// want q(a,b,c,d,e) = (all inside) V (all outside)
+						// (all in) V (all out) => q
+						fprintf(file_pointer, "%d %d %d %d %d %d 0\n", q(a,b,c,d,e), p(a,c), p(b,d), p(c,e), p(a,d), p(b,e));
+						fprintf(file_pointer, "%d %d %d %d %d %d 0\n", q(a,b,c,d,e), -p(a,c), -p(b,d), -p(c,e), -p(a,d), -p(b,e));
+						// the harder one: q => (all in) V (all out)
+						// it adds 20 clauses!  yikes!
+						// I got these by just expanding out the propositions until they were in CNF - don't actually have representation in mind
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(a,c), -p(b,d));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(a,c), -p(c,e));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(a,c), -p(a,d));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(a,c), -p(b,e));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(b,d), -p(a,c));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(b,d), -p(c,e));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(b,d), -p(a,d));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(b,d), -p(b,e));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(c,e), -p(a,c));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(c,e), -p(b,d));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(c,e), -p(a,d));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(c,e), -p(b,e));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(a,d), -p(a,c));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(a,d), -p(b,d));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(a,d), -p(c,e));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(a,d), -p(b,e));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(b,e), -p(a,c));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(b,e), -p(b,d));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(b,e), -p(c,e));
+						fprintf(file_pointer, "%d %d %d 0\n", -q(a,b,c,d,e), p(b,e), -p(a,d));
+	}}}}}
+
+	// finally, we add the knf clauses:
+
+	if(exact) {
+		fprintf(file_pointer, "k %d ", k);
+		for(int a = 0; a < N - 4; a++) {
+			for(int b = a+1; b < N - 3; b++) {
+				for(int c = b+1; c < N - 2; c++) {
+					for(int d = c+1; d < N - 1; d++) {
+						for(int e = d+1; e < N; e++) {
+							fprintf(file_pointer, "%d ", q(a,b,c,d,e));
+		}}}}}
+		fprintf(file_pointer, "0\n");
+	}
+	fprintf(file_pointer, "k %lu ", Nchoose5 - k);
+	for(int a = 0; a < N - 4; a++) {
+		for(int b = a+1; b < N - 3; b++) {
+			for(int c = b+1; c < N - 2; c++) {
+				for(int d = c+1; d < N - 1; d++) {
+					for(int e = d+1; e < N; e++) {
+						fprintf(file_pointer, "%d ", -q(a,b,c,d,e));
+	}}}}}
+	fprintf(file_pointer, "0\n");
+	
+	// symmetry breaking:
+	// first, we require that p(0,2) is true, breaking reflection along the circle
+	fprintf(file_pointer, "%d 0\n", p(0,2));
+
+	// we require 180 degree rotational symmetry, which shouldn't change the result in theory???
+	/*for(int j = 0; j < N; j++) {
+		for(int i = 0; i < j-1; i++) {
+			fprintf(file_pointer, "%d %d 0\n", p(i,j), -p((i + N/2) % N, (j + N/2) % N));
+			fprintf(file_pointer, "%d %d 0\n", -p(i,j), p((i + N/2) % N, (j + N/2) % N));
+	}}*/
+
+  // Close the file
+  fclose(file_pointer);
+
+  printf("Data written to %s successfully.\n", filename);
+
+  return 0; // Indicate successful execution
+}
