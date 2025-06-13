9228bd67668cf7b534ae5afd40c48dccb85e3bbb
hasitakv <hasitakv@HKwappytoppy.>
2025-06-12 20:15:31 -0400

    heatmap, need to fix symm

---
+++ isrotk6.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "bankers.c"
+
+const int N = 25;
+
+const int solutions = 80;
+
+bool rotstorage[solutions][N][N];
+
+int heatmap[N][N];
+
+unsigned long minBankers;
+
+// p(i,j) is whether the edge btwn i and j is inside or outside.
+// note that we only use this when i < j - 1, otherwise it's a useless var
+// but encoding it this way is way easier lol - we just set useless vars to true
+int p(int i, int j) {
+	// plus one is to move it from [0, N) to [1, N]
+	// don't think I need to do any casting as long as N < 31
+	unsigned long pos = ((1 << i) + (1 << j));
+	
+	return 1 + inverse(pos) - minBankers;
+}
+
+// get i given p
+int pi(int p) {
+	assert(p > 0);
+
+	unsigned long pos = compute(p - 1 + minBankers);
+	for(int i = 0; i < N; i++) {
+		if(pos & 1 == 1) return i;
+		pos = pos >> 1;
+	}
+	assert(false);
+	return -1;
+}
+
+// get j given p
+int pj(int p) {
+	assert(p > 0);
+
+	unsigned long pos = compute(p - 1 + minBankers);
+	bool foundOne = false;
+	for(int i = 0; i < N; i++) {
+		if(pos & 1 == 1 && foundOne) return i;
+		if(pos & 1 == 1 && !foundOne) foundOne = true;
+		pos = pos >> 1;
+	}
+	assert(false);
+	return -1;
+}
+
+//list edges inside from sample sol
+int main(){
+    FILE *fptr;
+
+	length = N;
+	minBankers = inverse((1 << N-1) + (1 << N-2));
+
+    // Open a file in read mode
+    fptr = fopen("k6s.sol", "r");
+
+    // Store the content of the file
+    char myString[1000000];
+		int count;
+    int curIndex;
+		int curTable;
+
+    // If the file exist
+    if(fptr != NULL) {
+
+        // Read the content and print it
+        while(fgets(myString, 1000000, fptr)) {
+            char * myPtr = strtok(myString, " ");
+            if(myPtr!=NULL && strcmp(myPtr, "v")==0){
+                while(myPtr != NULL) {
+                    char *endptr;
+                    long num = strtol(myPtr, &endptr, 10);
+
+                    if (*endptr != '\0') {
+                        //printf("Error: Invalid characters in string.\n");
+                        if(count != 0){
+                            printf("------Edges inside graph %d: %d------\n", curTable, count);
+														curTable++;
+                        }
+
+                        count = 0;
+                    } else {
+						//in the current table, store if the edge is inside
+						int i = pi(abs(num));
+						int j = pj(abs(num));
+
+						rotstorage[curTable][i][j] = (num > 0);
+						rotstorage[curTable][j][i] = (num > 0);
+						if(num > 0){
+							heatmap[i][j] ++;
+							heatmap[j][i] ++;
+						}
+                        if((num > 0) && i < j - 1) count++;
+                    }
+
+                    myPtr = strtok(NULL, " ");
+
+                }
+            }
+        }
+		//}
+
+    // If the file does not exist
+    } else {
+    printf("Not able to open the file.");
+    }
+
+    // Close the file
+    fclose(fptr);
+
+		// search through the 24choose2 pairs of tables to see which match
+		for(int a = 0; a < solutions; a++) {
+			// actually, first we're gonna check that a has all the properties we expect
+			for(int i = 0; i < N; i++) {
+				// true diagonal
+				//if( !rotstorage[a][i][i] ) printf("broke on graph %d at position (%d,%d)\n", a, i, i);
+				// true diagonal+1
+				assert( rotstorage[a][i][(i+1) % N] );
+				for(int j = 0; j < i; j++) {
+					// symmetric
+					assert( rotstorage[a][i][j] == rotstorage[a][j][i] );
+			}}
+
+
+
+			//checks post-bankers graph as expected
+			// printf("GRAPH %d: \n", a);
+
+			// for(int i = 0; i < N; i++){
+			// 	for(int j = 0; j < N; j++){
+			// 		if(rotstorage[a][i][j]){
+			// 			printf("1 ");
+			// 			heatmap[i][j] ++;
+			// 		} else {
+			// 			printf(". ");
+			// 		}
+			// 	}
+			// 	printf("\n");
+			// }
+
+
+			// okay, back to searching through pairs of graphs
+			for(int b = 0; b < solutions; b++) {
+				printf("Test (%d,%d)\n", a, b);
+				// don't want these though
+       		 	//if(a == b) continue;
+				// bigMatch stores if THERE EXISTS an x with the properties we want
+				bool bigMatch = false;
+				bool posmatchbys[N] = {false};
+				bool negmatchbys[N] = {false};
+				for(int x = 0; x < N; x++) {
+					// match is "is it a rotation (check ALL i,j)"
+					bool match = true;
+					// flipMatch is "is it a reflection (check ALL i,j)"
+					bool flipMatch = true;
+					for(int i = 0; i < N; i++) {
+						for(int j = 0; j < N; j++) {
+
+							if(rotstorage[a][i][j] != rotstorage[b][(i+x) % N][(j+x) % N])
+								match = false;
+							if(rotstorage[a][i][j] != rotstorage[b][(N-i+x) % N][(N-j+x) % N])
+								flipMatch = false;
+					}}
+					if(match || flipMatch) {
+						if(!(x == 0 && !flipMatch)) bigMatch = true;
+						if(match) posmatchbys[x] = true;
+						if(flipMatch) negmatchbys[x] = true;
+					}
+				}
+				if(bigMatch) {
+					printf("graph %d and graph %d match by:", a, b);
+					// this would mean the graphs are the exact same - because this doesn't trigger it means any 0s we see indicate a reflection!
+					//assert( !posmatchbys[0] );
+					for(int x = 0; x < N; x++) {
+						if(posmatchbys[x]) printf(" %d", x);
+						if(negmatchbys[x]) printf(" %d", x-N);
+					}
+					printf("\n");
+				}
+		}
+		}
+
+		printf("HEATMAP: \n");
+
+			for(int i = 0; i < N; i++){
+				for(int j = 0; j < N; j++){
+					if(heatmap[i][j] == 0){
+						printf("  .");
+					} else {
+						printf(" %d", heatmap[i][j]);
+					}
+				}
+				printf("\n");
+			}
+							
+}
